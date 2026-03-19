64480ba62df0c9859a19eaa816821eb2e0834455
Stepochkin <vladislav.stepochkin@philips.com>
2026-03-19 09:25:44 +0100

    3/17/2026

---
+++ 16-generic-functions.lisp
@@ -0,0 +1,31 @@
+(defgeneric draw (shape)
+  (:documentation "Draw the given shape."))
+
+(defgeneric withdraw (account amount)
+  (:documentation "Withdraw the specified amount from the account.
+Signal an error if the current balance is less than amount."))
+
+(defmethod withdraw ((account bank-account) amount)
+  (when (< (balance account) amount)
+    (error "Account overdrawn."))
+  (decf (balance account) amount))
+
+(defmethod withdraw ((account checking-account) amount)
+  (let ((overdraft (- amount (balance account))))
+    (when (plusp overdraft)
+      (withdraw (overdraft-account account) overdraft)
+      (incf (balance account) overdraft)))
+  (call-next-method))
+
+(defmethod withdraw ((proxy proxy-account) amount)
+  (withdraw (proxied-account proxy) amount))
+
+(defmethod withdraw ((account (eql *account-of-bank-president*)) amount)
+  (let ((overdraft (- amount (balance account))))
+    (when (plusp overdraft)
+      (incf (balance account) (embezzle *bank* overdraft)))
+  (call-next-method)))
+
+(defgeneric priority (job)
+  (:documentation "Return the priority at which the job should be run.")
+  (:method-combination + :most-specific-last))
